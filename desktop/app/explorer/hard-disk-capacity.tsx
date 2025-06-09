9addeeac0b958b5586413c0912274cfd48abe3c0
yangWs29 <781958371@qq.com>
2025-06-09 17:43:43 +0800

    添加显示磁盘空间

---
+++ desktop/app/explorer/hard-disk-capacity.tsx
@@ -0,0 +1,26 @@
+import disk from 'diskusage'
+import { Progress, Space } from 'antd'
+import { formatFileSize } from '@/app/explorer/file-utils'
+
+const HardDiskCapacity = async ({ currentPath = '' }: { currentPath: string }) => {
+  const { total, free } = await disk.check(currentPath)
+
+  return (
+    <Space style={{ width: '100%' }}>
+      <Space>
+        <span style={{ color: '#666' }}>已使用</span>
+        <span>{formatFileSize(total - free)}</span>
+      </Space>
+      <Progress
+        percent={Number((((total - free) / total) * 100).toFixed(2))}
+        status="active"
+        style={{ width: '100px' }}
+      />
+      <div style={{ color: '#666' }}>
+        {formatFileSize(free)} free of {formatFileSize(total)}
+      </div>
+    </Space>
+  )
+}
+
+export default HardDiskCapacity
--- desktop/app/explorer/layout.tsx
+++ desktop/app/explorer/layout.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Card, Space } from 'antd'
+import { Card, Divider, Space } from 'antd'
 import ExplorerBreadcrumb from '@/app/explorer/explorer-breadcrumb'
 import { ActionsBtn, EditProvider } from '@/app/explorer/edit-context'
 import { TerminalProvider } from '@/app/explorer/terminal-context'
+import HardDiskCapacity from '@/app/explorer/hard-disk-capacity'
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{ path?: string[] | undefined }>
+}) => {
+  const { path } = await params
+
   return (
     <EditProvider>
       <TerminalProvider>
@@ -15,9 +24,23 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
               <ActionsBtn />
             </Space>
           }
-          style={{ minHeight: '100vh', borderRadius: 0 }}
+          style={{ minHeight: '100vh', borderRadius: 0, display: 'flex', flexDirection: 'column' }}
+          styles={{ body: { flex: 1, display: 'flex', flexDirection: 'column' } }}
         >
-          {children}
+          <div style={{ width: '100%', height: '100%', flex: 1 }}>{children}</div>
+          <Divider />
+          <Space
+            style={{
+              width: '100%',
+              height: '20px',
+              flexShrink: 0,
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <HardDiskCapacity currentPath={path?.join('/') || '/'} />
+          </Space>
         </Card>
       </TerminalProvider>
     </EditProvider>
