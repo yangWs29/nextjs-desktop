a5c8f2349d628eda726f11a8889f974694989073
yangWs29 <781958371@qq.com>
2025-06-09 21:32:12 +0800

    修复中文编码

--- desktop/app/explorer/[...path]/page.tsx
+++ desktop/app/explorer/[...path]/page.tsx
@@ -3,7 +3,7 @@ import FileList from '@/app/explorer/components/file-list/file-list'
 const Page = async ({ params }: { params: Promise<{ path?: string[] | undefined }> }) => {
   const { path } = await params
 
-  const currentPath = path?.join('/') || ''
+  const currentPath = decodeURIComponent(path?.join('/') || '')
 
   return <FileList currentPath={currentPath} />
 }
