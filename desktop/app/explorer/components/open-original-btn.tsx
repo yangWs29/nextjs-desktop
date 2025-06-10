4ffb82bbf52297d90955ae77ec4d808ec6df5d65
yangWs29 <781958371@qq.com>
2025-06-10 17:36:36 +0800

    将按钮修改为 Link

--- desktop/app/explorer/components/open-original-btn.tsx
+++ desktop/app/explorer/components/open-original-btn.tsx
@@ -1,22 +1,15 @@
 'use client'
 import { ExportOutlined } from '@ant-design/icons'
-import { Button } from 'antd'
 import React from 'react'
 import { pathJoin } from '@/app/explorer/utils/file-utils'
 import { useEdit } from '@/app/explorer/edit-context'
+import Link from 'next/link'
 
 const OpenOriginalBtn = ({ file_path }: { file_path: string }) => {
   const { edit } = useEdit()
-  const handleOpenOriginal = (e: React.MouseEvent) => {
-    e.stopPropagation()
-    window.open(`${pathJoin('/explorer/static/', encodeURIComponent(file_path))}`, '_blank')
-  }
 
   return edit ? null : (
-    <Button
-      type="text"
-      onClick={handleOpenOriginal}
-      icon={<ExportOutlined />}
+    <Link
       style={{
         position: 'absolute',
         top: 3,
@@ -24,8 +17,13 @@ const OpenOriginalBtn = ({ file_path }: { file_path: string }) => {
         backgroundColor: 'rgba(0,0,0,0)',
         fontSize: '12px',
         zIndex: 1,
+        color: 'inherit',
       }}
-    />
+      href={pathJoin('/explorer/static/', encodeURIComponent(file_path))}
+      target="_blank"
+    >
+      <ExportOutlined />
+    </Link>
   )
 }
 
