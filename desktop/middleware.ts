16251c5f7d5ed8867af2a63ac2bb8fb86fd8a636
yangWs29 <781958371@qq.com>
2025-06-10 19:25:31 +0800

    修复中间件

--- desktop/middleware.ts
+++ desktop/middleware.ts
@@ -1,8 +1,14 @@
-import type { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const headers = new Headers(request.headers)
   headers.set('x-pathname', request.nextUrl.pathname)
+
+  return NextResponse.next({
+    request: {
+      headers,
+    },
+  })
 }
 
 export const config = {
