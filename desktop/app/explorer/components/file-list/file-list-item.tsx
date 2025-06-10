b7e2060caf1cf81a014708aa327fd3b5557ed317
yangWs29 <781958371@qq.com>
2025-06-10 13:40:20 +0800

    非目录情况下显示

--- desktop/app/explorer/components/file-list/file-list-item.tsx
+++ desktop/app/explorer/components/file-list/file-list-item.tsx
@@ -47,7 +47,7 @@ const FileListItem = ({ files, currentPath }: { files: File[]; currentPath: stri
       <div key={index} style={{ position: 'relative' }}>
         <MoreActionsModal currentPath={currentPath} file={file} baseDir={app_config.explorer_base_path} />
         <FileItemCheckbox hrefDir={path.join(currentPath || 'explorer', file.name)} />
-        <OpenOriginalBtn file_path={path.join(file.dirPath, file.name)} />
+        {!file.isDirectory && <OpenOriginalBtn file_path={path.join(file.dirPath, file.name)} />}
 
         {file.isDirectory ? (
           <Link href={path.join('/explorer', currentPath, encodeURIComponent(file.name))}>
